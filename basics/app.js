fae63f32bc8bf3d9033ca3ba5655ddc4a0437dfc
RajTangadi03 <rtangadi03@gmail.com>
2026-01-10 23:08:55 +0530

    initial commit

---
+++ basics/app.js
@@ -0,0 +1,8 @@
+var a = 10;
+
+// Two types of exports in ES6 modules - named export and default export
+// Named export - export {a, b, c};
+// Default export - export default a;
+
+export default a; // if we want to export a single value from a whole file then use default keyword
+// export {a}; // this is named export
---
+++ basics/script.js
@@ -0,0 +1,5 @@
+// before import and export use this in HTML file
+// <script type="module" src="script.js"></script>
+
+import a from "./app.js";
+console.log("Val a = ", a);
